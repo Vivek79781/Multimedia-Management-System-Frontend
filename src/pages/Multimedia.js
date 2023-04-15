33041c8fc05d886225d6f95c9b800bf94d3af772
Vivek79781 <vivekdbz248@gmail.com>
2023-04-15 12:46:40 +0530

    Edit Done

--- src/pages/Multimedia.js
+++ src/pages/Multimedia.js
@@ -116,6 +116,18 @@ const Multimedia = ({multimedias, getMultimedias, loading}) => {
         }
     }
 
+    const onSearchAuthor = value => {
+        console.log(value)
+        if(value === '')
+            setTableData(multimedias)
+        else {
+            let filteredData = multimedias.filter((data) => {
+                return data.name.toLowerCase().includes(value.toLowerCase())
+            })
+            setTableData(filteredData)
+        }
+    }
+
     return (
         (!loading) ? 
         <div className="container">
@@ -123,15 +135,23 @@ const Multimedia = ({multimedias, getMultimedias, loading}) => {
             <h1>All Public Multimedia</h1>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
                 <Search
-                    placeholder="Enter Description"
-                    onSearch={onSearchDescription}
+                    placeholder="Enter Author"
+                    onSearch={onSearchAuthor}
                     style={{ width: 300 }}
                 />
+                
                 <Search
                     placeholder="Enter Title"
                     onSearch={onSearchTitle}
                     style={{ width: 300 }}
                 />
+
+                <Search
+                    placeholder="Enter Description"
+                    onSearch={onSearchDescription}
+                    style={{ width: 300 }}
+                />
+                
             </div>
             
             <Table dataSource={tableData} columns={columns} />
