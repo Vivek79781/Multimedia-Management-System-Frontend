a565ffbaec258b9984db545230817e9c7620c680
Nikhil Saraswat <nikhilsaraswat@kgpian.iitkgp.ac.in>
2023-05-06 17:24:42 +0530

    commited

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -33,7 +33,7 @@ const Login = ({ login, isAuthenticated }) => {
             <p className="lead">< UserOutlined /> Sign In Your Account</p>
             <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
+                <input type="text" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
                 </div>
                 <div className="form-group">
                 <input
